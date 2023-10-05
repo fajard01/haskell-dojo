3d71b2cdec54b6b26539d4f94c949328b4178238
Danny Fajardo <fajardodanny@yahoo.co.uk>
2023-10-05 19:21:16 +0100

    HPFP and more sandbox

--- src/Sandbox.hs
+++ src/Sandbox.hs
@@ -1,3 +1,30 @@
 module Sandbox where
-    hello = "Hello World!"
 
+    counter x = let x = x + 1
+                    in 
+                        let x = x + 1
+                            in 
+                                x
+
+    counter' x = (\x ->
+                    (\x -> x) x + 1
+                 ) x + 1
+
+    ----------------
+    overwrite = let x = 2
+                    in 
+                        let x = 3
+                            in
+                                let x = 4
+                                    in
+                                        x 
+    
+    overwrite' = (\x ->
+                    (\x ->
+                        (\x -> x) 4
+                    ) 3
+                 ) 2 
+
+    ----------------
+    hello = "Hello World!"
+    
